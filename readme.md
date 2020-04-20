ae6ef1598a63dc7b58d3b0202b572ab99157e40f
Ritchie <lrq09@qq.com>
2020-04-21 01:06:03 +0800

    add vue core demo

---
+++ readme.md
@@ -0,0 +1,14 @@
+## Vue 2 学习笔记
+
+## 调试技巧
+
+- 如何在console中修改vue中的变量
+    - 在chrome-dev中打开vue-tab
+    - 选择vue绑定的div
+    - `$vm.targeVariable`
+    
+## 参考资料
+
+- https://cn.vuejs.org/v2/guide/installation.html
+- https://github.com/justjavac/awesome-wechat-weapp
+- [vue dev tools doc](https://github.com/vuejs/vue-devtools)
--- start.js
+++ start.js
@@ -1,15 +1,47 @@
-new Vue({
+Vue.component("todo-item",{
+  template: '<li>  this is a componet time: https://scrimba.com/g/gvuedocs </li>'
+});
+
+var app = new Vue({
   el: '#example-3',
   data: {
     name: 'L R Q',
     items: [
       { message: 'Foo' },
       { message: 'Bar' }
-    ]
+    ],
+    seen: true,
+    model_msg: "this is two way bounded demo"
   },
   methods: {
     say: function (message) {
       alert(message)
+    },
+    reverseName: function(){
+      this.name = this.name.split(" ").reverse().join(" ");
+      console.log("click on reverse button");
     }
+  }
+})
+
+// control in js
+app.items.push({message:"add in js"});
+
+Vue.component("todo-item2", {
+  // todo-item 组件现在接受一个
+  // "prop"，类似于一个自定义 attribute。
+  // 这个 prop 名为 todo。
+  props: ['todo'],
+  template: '<li> {{todo.text}}</li>'
+});
+
+var app2 = new Vue({
+  el: "#app2",
+  data:{
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
   }
 })
