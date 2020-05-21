a08afd2b8908afe4e947be5bd1b7e3b599ec4e80
Ritchie <lrq09@qq.com>
2020-05-22 07:51:18 +0800

    add component

--- readme.md
+++ readme.md
@@ -1,12 +1,34 @@
 ## Vue 2 学习笔记
 
+###  常用指令
+
+- v-bind 绑定属性
+    - `<a :href="url">...</a>`
+- v-on 监听事件
+    - `<a @click="doSomething">...</a>`
+    - 中括号指定属性名，动态绑定参数
+    - 动态绑定约束：不使用表达式，全小写
+    - `.`修饰符，添加额外的绑定信息 `<form v-on:submit.prevent="onSubmit">...</form>` 
+
+### vue的默认属性以$为前缀
+
+- `vm.$data === data`
+- `vm.$el === document.getElementById('example') // => true`
+
+```js
+// 默认实例方法
+vm.$watch('a', function (newValue, oldValue) {
+  // 这个回调将在 `vm.a` 改变后调用
+})
+```
+
 ## 调试技巧
 
 - 如何在console中修改vue中的变量
     - 在chrome-dev中打开vue-tab
     - 选择vue绑定的div
     - `$vm.targeVariable`
-    
+
 ## 参考资料
 
 - https://cn.vuejs.org/v2/guide/installation.html
--- start.js
+++ start.js
@@ -2,6 +2,25 @@ Vue.component("todo-item",{
   template: '<li>  this is a componet time: https://scrimba.com/g/gvuedocs </li>'
 });
 
+Vue.component("blog-post", {
+  props: ['post'],
+  template: `<div class="blog-post">
+    <h3> {{post.text}}</h3>
+    <button v-on:click="$emit('enlarge-text')">
+        Enlarge text
+      </button>
+  </div>
+  `
+});
+
+Vue.component('alert-box', {
+  template: `
+    <div class="demo-alert-box">
+      <strong>Error!</strong>
+      <slot></slot>
+    </div>
+  `
+});
 var app = new Vue({
   el: '#example-3',
   data: {
@@ -10,7 +29,12 @@ var app = new Vue({
       { message: 'Foo' },
       { message: 'Bar' }
     ],
+    checkedNames:[],
     seen: true,
+    awesome: 123,
+    counter:0,
+    loginType: "",
+    message:"multi lines input",
     model_msg: "this is two way bounded demo"
   },
   methods: {
@@ -20,6 +44,25 @@ var app = new Vue({
     reverseName: function(){
       this.name = this.name.split(" ").reverse().join(" ");
       console.log("click on reverse button");
+    }
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 vm 实例
+      return this.message.split('').reverse().join('')
+    },
+    fullName: {
+      // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
     }
   }
 })
@@ -42,6 +85,27 @@ var app2 = new Vue({
       { id: 0, text: 'Vegetables' },
       { id: 1, text: 'Cheese' },
       { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
+    ],
+    postFontSize:1,
+    a : 8080,
+    number:10,
+    ok:true,
+    message:"test mustache",
+    id:666,
+    rawHtml: '<span style="color:red">This should be red</span>'
+  },
+  create: function(){ // 实例被创建后 调用
+    // 不要在选项属性或回调上使用箭头函数，
+    // 比如 created: () => console.log(this.a) 
+    // 或 vm.$watch('a', newValue => this.myMethod())。因为箭头函数并没有 this，
+    // this 会作为变量一直向上级词法作用域查找，直至找到为止
+    // this = app2
+    console.log('a is: ' + this.a);
+  },
+  methods: {
+    onEnlargeText: function (enlargeAmount) {
+      //debugger; 发送事件时，如果没有传参数，就是undefined
+      this.postFontSize += enlargeAmount;
+    }
   }
 })
